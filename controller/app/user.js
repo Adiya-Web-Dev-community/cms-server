ce30f3040345c874c94046c8e96b0d4c135a6ebc
harshadashirsat6 <harshadaa1997@gmail.com>
2024-04-08 12:30:40 +0530

    modify page.data

--- controller/app/user.js
+++ controller/app/user.js
@@ -9,54 +9,40 @@ const fetchAllUserProjects = async (req, res) => {
     return res.send({ success: false, msg: "User Id not found" });
   }
   try {
-    const allProjects = await UserProject.find({ userId:userId });
+    const allProjects = await UserProject.find({ userId: req.accountId });
     return res.send({ list: allProjects, listLength: allProjects.length });
   } catch (err) {
     return res.send({ success: true, msg: `error: ${err.message}` });
   }
 };
 
+//fetch template data by id
+const fetchProject = async (req, res) => {
+  const { projectId } = req.params;
+  if (!projectId) {
+    return res.send({ success: false, msg: "Project id not found" });
+  }
+  try {
+    const isProject = await UserProject.findOne({ _id: projectId })
+      .populate("userId")
+      .populate("pages");
+    if (!isProject) {
+      return res.send({ success: false, msg: "Project not found" });
+    }
+
+    return res.send({
+      success: true,
+      msg: "Template data fetch successfully",
+      isProject,
+    });
+  } catch (err) {
+    return res.send({ success: false, msg: `catch error: ${err.message}` });
+  }
+};
+
 //create blank project
-// const createBlankProject = async (req, res) => {
-//   const { projectName, category } = req.body;
-//   if (!projectName) {
-//     return res.send({ success: false, msg: "Project name cannot be empty!" });
-//   }
-//   if (!category) {
-//     return res.send({
-//       success: false,
-//       msg: "Please select category to cretae new template",
-//     });
-//   }
-//   try {
-//     //create template
-//     const newProject = await UserProject.create({
-//       ...req.body,
-//       userId: req.accountId,
-//     });
-//     //create default home page
-//     const newPage = await UserProjectPage.create({
-//       title: "Home",
-//       projectId: newProject._id,
-//     });
-
-//     //add home page ID to template
-//     newProject.pages.push(newPage._id);
-//     await newProject.save();
-
-//     return res.send({
-//       success: true,
-//       msg: "New project and default home page created",
-//       newProject,
-//       newPage,
-//     });
-//   } catch (err) {
-//     return res.send({ success: false, msg: `catch error:${err.message}` });
-//   }
-// };
-
 const createBlankProject = async (req, res) => {
-  const { projectName, category, userId } = req.body;
+  const { projectName, category } = req.body;
   if (!projectName) {
     return res.send({ success: false, msg: "Project name cannot be empty!" });
   }
@@ -66,17 +52,11 @@ const createBlankProject = async (req, res) => {
       msg: "Please select category to cretae new template",
     });
   }
-  if (!userId) {
-    return res.send({
-      success: false,
-      msg: "User Id not found",
-    });
-  }
   try {
     //create template
     const newProject = await UserProject.create({
       ...req.body,
-      userId: userId,
+      userId: req.accountId,
     });
     //create default home page
     const newPage = await UserProjectPage.create({
@@ -100,38 +80,8 @@ const createBlankProject = async (req, res) => {
 };
 
 //create project from template
-// const createProjectFromTemplate = async (req, res) => {
-//   const { originalTemplateId } = req.body;
-
-//   //find original template first
-//   const originalTemplate = await Template.findOne({
-//     _id: originalTemplateId,
-//   }).populate("pages");
-
-//   //create new project for user by taking reference of template
-//   const newProject = new UserProject({
-//     projectName: originalTemplate.templateName,
-//     category: originalTemplate.category,
-//     appIcon: originalTemplate.appIcon,
-//     BottomTabData: originalTemplate.BottomTabData,
-//     userId: req.accountId,
-//     pages: [],
-//   });
-//   await newProject.save();
-
-//   for (let page of originalTemplate.pages) {
-//     const newPage = await UserProjectPage.create({
-//       title: page.title,
-//       data: page.data,
-//       projectId: newProject._id,
-//     });
-//     newProject.pages.push(newPage._id);
-//     await newProject.save();
-//   }
-//   return res.send({ newProject });
-// };
 const createProjectFromTemplate = async (req, res) => {
-  const { originalTemplateId, userId } = req.body;
+  const { originalTemplateId } = req.body;
 
   //find original template first
   const originalTemplate = await Template.findOne({
@@ -144,7 +94,7 @@ const createProjectFromTemplate = async (req, res) => {
     category: originalTemplate.category,
     appIcon: originalTemplate.appIcon,
     BottomTabData: originalTemplate.BottomTabData,
-    userId: userId,
+    userId: req.accountId,
     pages: [],
   });
   await newProject.save();
@@ -159,6 +109,52 @@ const createProjectFromTemplate = async (req, res) => {
     await newProject.save();
   }
   return res.send({ newProject });
+};
+
+//add bottom data form template
+const addBottomData = async (req, res) => {
+  const { projectId } = req.params;
+  if (!projectId) {
+    return res.send({ success: false, msg: "Cannot find template Id" });
+  }
+  const { data } = req.body;
+  try {
+    const isProject = await UserProject.findOne({ _id: projectId });
+    for (let obj of data) {
+      isProject.BottomTabData.push(obj);
+    }
+    await isProject.save();
+    return res.send({ success: true, msg: "Footer data saved", isProject });
+  } catch (err) {
+    return res.send({ success: false, msg: `error:${err.message}` });
+  }
+};
+
+//change project title and appIcon
+const changeProjectData = async (req, res) => {
+  const { projectId } = req.params;
+  if (!projectId) {
+    return res.send({ success: false, msg: "Project Id not found" });
+  }
+
+  try {
+    const isProject = await UserProject.findOne({ _id: projectId });
+    if (req.body.projectName) {
+      isProject.projectName = req.body.projectName;
+      await isProject.save();
+    }
+    if (req.body.appIcon) {
+      isProject.appIcon = req.body.appIcon;
+      await isProject.save();
+    }
+    return res.send({
+      success: true,
+      msg: "Project has been updated",
+      data: isProject,
+    });
+  } catch (err) {
+    return res.send({ success: true, msg: `error:${err.message}` });
+  }
 };
 
 //create new page
@@ -189,57 +185,6 @@ const createNewProjectPage = async (req, res) => {
   }
 };
 
-//fetch template data by id
-const fetchProject = async (req, res) => {
-  const { projectId } = req.params;
-  if (!projectId) {
-    return res.send({ success: false, msg: "Project id not found" });
-  }
-  try {
-    const isProject = await UserProject.findOne({ _id: projectId })
-      .populate("userId")
-      .populate("pages");
-    if (!isProject) {
-      return res.send({ success: false, msg: "Project not found" });
-    }
-
-    return res.send({
-      success: true,
-      msg: "Template data fetch successfully",
-      isProject,
-    });
-  } catch (err) {
-    return res.send({ success: false, msg: `catch error: ${err.message}` });
-  }
-};
-
-//change project title
-const changeProjectData = async (req, res) => {
-  const { projectId } = req.params;
-  if (!projectId) {
-    return res.send({ success: false, msg: "Project Id not found" });
-  }
-
-  try {
-    const isProject = await UserProject.findOne({ _id: projectId });
-    if (req.body.projectName) {
-      isProject.projectName = req.body.projectName;
-      await isProject.save();
-    }
-    if (req.body.appIcon) {
-      isProject.appIcon = req.body.appIcon;
-      await isProject.save();
-    }
-    return res.send({
-      success: true,
-      msg: "Project has been updated",
-      data: isProject,
-    });
-  } catch (err) {
-    return res.send({ success: true, msg: `error:${err.message}` });
-  }
-};
-
 //change page name
 const changePageTitle = async (req, res) => {
   const { pageId } = req.params;
@@ -267,10 +212,105 @@ const changePageTitle = async (req, res) => {
   }
 };
 
+//insert page data
+const insertPageData = async (req, res) => {
+  const { projectId, pageId, insertData } = req.body;
+  const randomId = Math.floor(Math.random() * (999999 - 10 + 1)) + 10;
+  try {
+    const isPage = await UserProjectPage.findOne({
+      $and: [{ projectId: projectId }, { _id: pageId }],
+    });
+    for (let obj of insertData) {
+      isPage.data.push({ ...obj, id: randomId });
+    }
+    await isPage.save();
+    return res.send({
+      projectId,
+      pageId,
+      isPage,
+      length: isPage.data.length,
+    });
+  } catch (err) {
+    return res.send({ success: false, msg: `error:${err.message}` });
+  }
+};
+
 //delete subData
-const deleteSubData = async (req, res) => {
-  const { pageId, subDataId } = req.params;
-  console.log(deleteSubData);
+const deletePageData = async (req, res) => {
+  const { pageId, subDataId } = req.body;
+  try {
+    const isPage = await UserProjectPage.findOne({ _id: pageId });
+    if (!isPage) {
+      return res.send({
+        success: false,
+        msg: "Page cannot found with give id",
+      });
+    }
+
+    isPage.data = isPage.data.filter((obj) => obj.id !== subDataId);
+    await isPage.save();
+    return res.send({
+      success: false,
+      msg: "Data deleted successfully",
+      isPage,
+    });
+  } catch (err) {
+    return res.send({ success: false, msg: `error : ${err.message}` });
+  }
+};
+
+//modify image /title of data
+const modifyDataTitleAndImage = async (req, res) => {
+  const { pageId, subDataId } = req.body;
+  let updateData = {};
+  if (req.body.image) {
+    updateData.image = req.body.image;
+  }
+  if (req.body.title) {
+    updateData.title = req.body.title;
+  }
+  try {
+    const isPage = await UserProjectPage.findOne({ _id: pageId });
+    const obj = isPage.data.find((obj) => obj.id === subDataId);
+    if (req.body.title) {
+      obj.title = req.body.title;
+    }
+    if (req.body.image) {
+      obj.image = req.body.image;
+    }
+    await isPage.save();
+    return res.send({ success: true, msg: "sub data found", isPage });
+  } catch (err) {
+    return res.send({ success: false, msg: `error :${err.message}` });
+  }
+};
+
+//insert list item
+const insertListItem = async (req, res) => {
+  const { pageId, subDataId, listItemData } = req.body;
+  try {
+    const filter = { _id: pageId, "data.id": subDataId };
+    const update = {
+      $push: { "data.$.ListItems": listItemData },
+    };
+    const options = { new: true };
+    const updatedPage = await UserProjectPage.findOneAndUpdate(
+      filter,
+      update,
+      options
+    );
+    if (!updatedPage) {
+      return res
+        .status(404)
+        .send({ success: false, msg: "Page or Subdata not found" });
+    }
+
+    return res.send(updatedPage);
+  } catch (err) {
+    return res
+      .status(500)
+      .send({ success: false, msg: `error: ${err.message}` });
+  }
 };
 
 module.exports = {
@@ -278,8 +318,12 @@ module.exports = {
   createBlankProject,
   createProjectFromTemplate,
   createNewProjectPage,
+  insertPageData,
+  deletePageData,
+  modifyDataTitleAndImage,
+  insertListItem,
   fetchProject,
   changeProjectData,
   changePageTitle,
-  deleteSubData,
+  addBottomData,
 };
--- router/app/user.js
+++ router/app/user.js
@@ -6,23 +6,35 @@ const {
   createBlankProject,
   createProjectFromTemplate,
   createNewProjectPage,
+  insertPageData,
+  deletePageData,
+  modifyDataTitleAndImage,
+  insertListItem,
   fetchProject,
   changeProjectData,
   changePageTitle,
-  deleteSubData,
+  addBottomData,
 } = require("../../controller/app/user");
 
-//! router.get("/fetch-user-projects", middleware, fetchAllUserProjects);
-//! router.post("/create-blank-project",middleware, createBlankProject);
-//! router.get("/create-project-from-template", middleware, createBlankProject);
-
 //? check later token not wokring for native developer
-router.get("/fetch-user-projects/:userId", fetchAllUserProjects);
-router.post("/create-blank-project", createBlankProject);
-router.post("/create-new-project-page/:projectId", createNewProjectPage);
-router.get("/create-project-from-template", createProjectFromTemplate);
+router.get("/fetch-user-projects", middleware, fetchAllUserProjects);
 router.get("/fetch-project/:projectId", fetchProject);
+
+router.post("/create-blank-project", middleware, createBlankProject);
+router.get(
+  "/create-project-from-template",
+  middleware,
+  createProjectFromTemplate
+);
+router.post("/addBottomData/:projectId", addBottomData);
 router.patch("/change-project-data/:projectId", changeProjectData);
+//page data apis
+router.post("/create-new-project-page/:projectId", createNewProjectPage);
+router.post("/insert-project-page-data", insertPageData);
 router.patch("/change-page-title/:pageId", changePageTitle);
-router.delete("/delete-subData/:pageId/:subDataId", deleteSubData);
+router.delete("/delete-page-data", deletePageData);
+router.patch("/modify-data-title-image", modifyDataTitleAndImage);
+//page data ->list items apis
+router.post("/insert-project-page-list-item", insertListItem);
+
 module.exports = router;
